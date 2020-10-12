804a7e477111b2ddd8c036c2e3fab43b8406a8ff
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2020-10-12 19:25:06 +0300

    возвращает обратно файл слайдера

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import "./sass/main.scss";
 import * as markup from "./js/markup.js"
 import * as burger from './js/burger.js';
+import * as slider from './js/slider-dremka.js';
+
 import * as formSend from './js/form-send.js';
 
-import * as slider from './slider-dremka.js';
+
