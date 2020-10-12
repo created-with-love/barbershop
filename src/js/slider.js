05a3d32b531bb35b3c1dd679cf7fbf034b877f87
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2020-10-12 21:07:43 +0300

    исправляет ошибки слайдера

--- src/js/slider.js
+++ src/js/slider.js
@@ -3,15 +3,15 @@
   const sliderThirdItem = document.querySelector('.slide-3');
   const sliderBtnNext = document.querySelector('[data-btn-next]');
   const sliderBtnPrev = document.querySelector('[data-btn-prev]');
-  const activeSlide = document.querySelector('[data-active]');
+  const activeSlide = document.querySelector('active-slide');
   const sliderItems = document.querySelectorAll('[data-item]');
     
   const firstRadioBtn = document.querySelector('#first-slider-btn');
   const secondRadioBtn = document.querySelector('#second-slider-btn');
   const thirdRadioBtn = document.querySelector('#third-slider-btn');
     
-  const nextActiveSlide = activeSlide.nextElementSibling;
-  const prevActiveSlide = activeSlide.previousElementSibling;
+  const nexActiveSlide =activeSlide.nextElementSibling;
+  const preActiveSlide =activeSlide.previousElementSibling;
 
 
 function onSideSliderNavClick(e) {
@@ -19,24 +19,24 @@ function onSideSliderNavClick(e) {
   if (e.target.nodeName !== 'INPUT') return;
 
   sliderItems.forEach(item => {
-    item.classList.replace('[data-active]', 'visuallyhidden');
+    item.classList.replace('active-slide', 'hidden');
   });
 
   switch (e.target.dataset.sliderBtn) {
     case 'first':
-      sliderFirstItem.classList.replace('visuallyhidden', '[data-active]');
+      sliderFirstItem.classList.replace('hidden', 'active-slide');
       e.target.previousElementSibling.checked = true;
       sliderBtnNext.disabled = false;
       sliderBtnPrev.disabled = true;
       break;
     case 'second':
-      sliderSecondItem.classList.replace('visuallyhidden', '[data-active]');
+      sliderSecondItem.classList.replace('hidden', 'active-slide');
       e.target.previousElementSibling.checked = true;
       sliderBtnNext.disabled = false;
       sliderBtnPrev.disabled = false;
       break;
     case 'third':
-      sliderThirdItem.classList.replace('visuallyhidden', '[data-active]');
+      sliderThirdItem.classList.replace('hidden', 'active-slide');
       e.target.previousElementSibling.checked = true;
       sliderBtnNext.disabled = true;
       sliderBtnPrev.disabled = false;
@@ -49,51 +49,51 @@ function onSliderBtnClick(e) {
   if (e.target.nodeName !== 'BUTTON') return;
 
   if (e.target === sliderBtnNext) {
-    if (sliderFirstItem === activeSlide) {
+    if (sliderFirstItem ===activeSlide) {
       sliderBtnNext.disabled = false;
       sliderBtnPrev.disabled = false;
       secondRadioBtn.checked = true;
     }
 
-    if (sliderSecondItem === activeSlide) {
+    if (sliderSecondItem ===activeSlide) {
       sliderBtnNext.disabled = true;
       sliderBtnPrev.disabled = false;
       thirdRadioBtn.checked = true;
     }
 
-    if (sliderThirdItem === activeSlide) return;
+    if (sliderThirdItem ===activeSlide) return;
 
-    activeSlide.classList.replace('[data-active]', 'visuallyhidden');
-    nextActiveSlide.classList.replace('visuallyhidden', '[data-active]');
+activeSlide.classList.replace('active-slide', 'hidden');
+    nexActiveSlide.classList.replace('hidden', 'active-slide');
   }
 
   if (e.target === sliderBtnPrev) {
-    if (sliderFirstItem === activeSlide) return;
+    if (sliderFirstItem ===activeSlide) return;
 
-    if (sliderSecondItem === activeSlide) {
+    if (sliderSecondItem ===activeSlide) {
       sliderBtnNext.disabled = false;
       sliderBtnPrev.disabled = true;
       firstRadioBtn.checked = true;
     }
 
-    if (sliderThirdItem === activeSlide) {
+    if (sliderThirdItem ===activeSlide) {
       sliderBtnNext.disabled = false;
       sliderBtnPrev.disabled = false;
       secondRadioBtn.checked = true;
     }
 
-    activeSlide.classList.replace('[data-active]', 'visuallyhidden');
-    prevActiveSlide.classList.replace('visuallyhidden', '[data-active]');
+activeSlide.classList.replace('active-slide', 'hidden');
+    preActiveSlide.classList.replace('hidden', 'active-slide');
   }
 }
 
 
 // установка первого слайда открытым
 
-sliderFirstItem = document.querySelector('.slide-1');
-sliderBtnsList = document.querySelector('.hero-section__buttons');
+const sliderBtnsList = document.querySelector('.hero-section__buttons');
+const sliderRadioList = document.querySelector('.slider-lines__form');
 
-sliderFirstItem.classList.replace('visuallyhidden', '[data-active]');
+sliderFirstItem.classList.replace('hidden', 'active-slide');
 document.querySelector('#first-slider-btn').checked = true;
 
 // слайдер - переключение через радио-кнопки
