4268a12e3e9f98e464fd8c5f025541417f3d0a89
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2020-10-12 21:34:47 +0300

    пробую старый слайдер обратно

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import "./sass/main.scss";
 import * as markup from "./js/markup.js"
 import * as burger from './js/burger.js';
-// import * as slider from './js/slider-dremka.js';
-import * as slider2 from './js/slider.js';
+import * as slider from './js/slider-dremka.js';
+// import * as slider2 from './js/slider.js';
 
 import * as formSend from './js/form-send.js';
 
